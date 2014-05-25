9e6038c9d8123ef84c85ee3934948734298657df
xylim2011 <xylim.2011@sis.smu.edu.sg>
2014-05-25 21:35:49 +0800

    Second commit
    Signed-off-by: xylim2011 <xylim.2011@sis.smu.edu.sg>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Chi-E");
+		System.out.println("FYP");
+		for(int i=0;i<3;i++){
+			System.out.println("*");
+		}
 
 	}
 
